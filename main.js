7c33fd18670858145fff848fb01ddbbb9c609875
Anshuman Chhapolia <achhap.10.01@gmail.com>
2020-01-26 20:05:04 +0530

    Add print option
    Signed-off-by: Anshuman Chhapolia <achhap.10.01@gmail.com>

--- main.js
+++ main.js
@@ -4,7 +4,6 @@ const {spawn, exec} = require('child_process');
 const appMenu = require('./menu');
 
 let mainWindow;
-let dirSelector;
 let improcess;
 
 function createMainWindow() {
@@ -21,13 +20,13 @@ function createMainWindow() {
     });
 
     let selectorOptions = {
-        defaultPath: __dirname,
+        defaultPath: app.getPath('desktop'),
         buttonLabel: "Select DataFile",
         filter: [{name:"IM Database",extensions:["imdb"]}],
         properties: ['openFile']
     };
         let filePath = dialog.showOpenDialogSync(mainWindow, selectorOptions);
-    console.log(filePath)
+    console.log(filePath);
     if (filePath === undefined) {
         app.quit()
         return
@@ -39,12 +38,11 @@ function createMainWindow() {
         }
     }
 
-    mainWindow.webContents.openDevTools()
+    // mainWindow.webContents.openDevTools();
 
 
     Menu.setApplicationMenu(appMenu);
     mainWindow.loadURL("http:localhost:4001");
-
 
     mainWindow.on('closed', () => {
         mainWindow = null;  // Darwin
--- menu.js
+++ menu.js
@@ -1,16 +1,19 @@
-const {app, Menu} = require("electron");
+const {app, Menu, dialog} = require("electron");
+const fs = require('fs');
 
-const domain='http://localhost:4001';
+const domain = 'http://localhost:4001';
 
 const template = [
     {
         label: 'File',
         submenu: [
             {id: 'groups', label: 'Groups', click: loadUrl('/groups')},
-            {id: 'products', label: 'Products', click:loadUrl('/products')},
+            {id: 'products', label: 'Products', click: loadUrl('/products')},
             {type: 'separator'},
-            {id: 'users', label: 'Manage Users',click:loadUrl('/users')},
-            {id: 'logout', label: 'Logout',click:loadUrl('/logout')},
+            {id: 'print', label: 'Print', click: onClickPrint},
+            {type: 'separator'},
+            {id: 'users', label: 'Manage Users', click: loadUrl('/users')},
+            {id: 'logout', label: 'Logout', click: loadUrl('/logout')},
             {type: 'separator'},
             {role: 'quit'}
         ]
@@ -18,26 +21,58 @@ const template = [
     {
         label: 'View',
         submenu: [
-            {id:'daily', label:'Daily Report',click:loadUrl('/report')},
-            {id:'stocks', label:'Stocks'},
-            {id:'pstocks',label:'Product Stocks'},
-            {id:'lpayment',label:'Labour Payment'},
+            {id: 'daily', label: 'Daily Report', click: loadUrl('/report')},
+            {id: 'stocks', label: 'Stocks', click: loadUrl('/stock')},
+            {id: 'pstocks', label: 'Product Stocks', click: loadUrl('/stock/product')},
+            {id: 'lpayment', label: 'Labour Payment', click: loadUrl('/labourpayment')},
         ]
     },
     {
-        label:'entry',
+        label: 'entry',
         submenu: [
-            {id:'dne', label:'Daily Entry'},
-            {id:'de', label:'Dispatch Entry'}
+            {id: 'dne', label: 'Daily Entry', click: loadUrl('/entry')},
+            {id: 'de', label: 'Dispatch Entry', click: loadUrl('/dispatch')}
         ]
     }
 
 ];
 
-function loadUrl(url){
-    return (event, focusedWindow, focusedWebContents)=>{
-        focusedWindow.loadURL(domain+url);
+function loadUrl(url) {
+    return (event, focusedWindow, focusedWebContents) => {
+        focusedWindow.loadURL(domain + url);
     }
+}
+
+function onClickPrint(event, focusedWindow, focusedWebContents) {
+
+    let saveOptions = {
+        title: 'Print to PDF',
+        defaultPath: app.getPath('desktop'),
+        filters: [{name: 'PDF', extensions: ['pdf']}]
+    };
+    let fileRegex = /(.pdf)$/i;
+    let regex = RegExp(fileRegex);
+
+    savePath = dialog.showSaveDialogSync(focusedWindow, saveOptions);
+    if (savePath !== undefined) {
+        if (!regex.test(savePath)) {
+            savePath = savePath + '.pdf';
+        }
+        let landscape = isLandScape(focusedWindow.getURL());
+        console.log(focusedWindow.getURL());
+        focusedWindow.webContents.printToPDF({pageSize:'A4',landscape:landscape}).then(data => {
+            fs.writeFile(savePath, data, (error) => {
+                if (error) throw error;
+                console.log('Write PDF successfully.')
+            })
+        }).catch(error => {
+            console.log(error)
+        });
+    }
+}
+
+function isLandScape( url){
+    return url.includes('labour');
 }
 
 let menu = Menu.buildFromTemplate(template);
